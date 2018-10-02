47d31d8100e96bafe1edf1345f6a274c1cee56ce
Atras Tudhi <atras.r@gmail.com>
2018-10-02 15:47:03 +0700

    revisi

--- exercises4.js
+++ exercises4.js
@@ -2,10 +2,50 @@ var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca
 
 input.splice(1, 4, "Roman Alamsyah Elsharawi", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro");
 
+var pisah = [];
+pisah = input[3].split("/");
+
 
 function dataHandling(input) {
  console.log(input)
- console.log(input[3])
+
+  switch(pisah[1]) {
+    case '01' : console.log('Januari');
+      break;
+    case '02' : console.log('Februari');
+      break;
+    case '03' : console.log('Maret');
+      break;
+    case '04' : console.log('April');
+      break;
+    case '05' : console.log('Mei');
+      break;
+    case '06' : console.log('Juni');
+      break;
+    case '07' : console.log('Juli');
+      break;
+    case '08' : console.log('Agustus');
+      break;
+    case '09' : console.log('September');
+      break;
+    case '10' : console.log('Oktober');
+      break;
+    case '11' : console.log('November');
+      break;
+    case '12' : console.log('Desember');
+      break;
+  } console.log(pisah[1])
 }
 
 dataHandling(input)
+
+
+pisah.sort(function(a, b){return b-a});
+console.log(pisah);
+
+var gabung = pisah.join('-');
+console.log(gabung);
+
+var limit = String(input[1])
+var limit = input[1].slice(0, 15);
+console.log(limit);
